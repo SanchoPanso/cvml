8d7cfe98d070d6b0e7e29a868c1c9e7191226d5a
unknown <20alexchernik@gmail.com>
2022-10-02 22:50:23 +0300

    test: add test for detection dataset and annotation editor

---
+++ data_science_tools/tests/detection/test_annotation_editor.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))))
+
+from data_science_tools.core.bounding_box import BoundingBox
+from data_science_tools.detection.dataset.label_editor import AnnotationEditor, Annotation
+
+
+def test_change_classes_by_id():
+    annotation = Annotation()
+    annotation.classes = ['0', '1', '2', '3', '4']
+    annotation.bounding_boxes['image_name'] = [
+        BoundingBox(0, 25, 25, 25, 25),
+        BoundingBox(1, 25, 25, 25, 25),
+        BoundingBox(2, 25, 25, 25, 25),
+        BoundingBox(3, 25, 25, 25, 25),
+    ]
+    
+    editor = AnnotationEditor()
+    id_changes = {0: None, 1: 4}
+    new_annotation = editor.change_classes_by_id(annotation, id_changes)
+
+    assert new_annotation.bounding_boxes['image_name'][0].get_class_id() == 4
+    assert new_annotation.bounding_boxes['image_name'][1].get_class_id() == 2
+    assert new_annotation.bounding_boxes['image_name'][2].get_class_id() == 3
+
+
