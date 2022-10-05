b41b1c8f1e6c7235f6c6584d61cd417ca7b3aaca
student2 <20alexchernik@gmail.com>
2022-10-05 18:11:07 +0300

    refactor: add setup

---
+++ data_science_tools/core/annotation.py
@@ -0,0 +1,18 @@
+from typing import List, Dict
+from data_science_tools.core.bounding_box import BoundingBox
+
+
+class Annotation:
+    """
+    Representation of the detection annotation of some dataset.
+    """
+    def __init__(self, 
+                 classes: List[str] = None, 
+                 bounding_boxes: Dict[str, List[BoundingBox]] = None):
+        """
+        :classes: list of class names
+        :bounding_boxes: dict with keys - image names and values - list of bounding boxes on this image
+        """
+        
+        self.classes = [] if classes is None else classes
+        self.bounding_boxes = {} if bounding_boxes is None else bounding_boxes
