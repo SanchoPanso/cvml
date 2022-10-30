e84a55a0fb1647e17eaa7865f66457f4d36ab762
student2 <20alexchernik@gmail.com>
2022-10-30 21:27:29 +0300

    chore: add samples

--- samples/3010_create_dataset.py
+++ samples/3010_create_dataset.py
@@ -9,15 +9,29 @@ from cvml.dataset_linking.main_file_ops import mv_dir, rm_dir
 
 
 def main():
-    src_dir = r''
-    dst_dir = r''
+    src_dir = '/home/student2/datasets/source/annotations TMK 30.10'
+    dst_dir = '/home/student2/datasets/raw/TMK_3010'
 
     zip_files = glob.glob(os.path.join(src_dir, '*.zip'))
+    print(dst_dir)
     
     for zip_file in zip_files:
         unzip_to(os.path.join(src_dir, zip_file), 
                  os.path.join(dst_dir, os.path.splitext(zip_file)[0]))
-    # create_comet_1(source_dir, raw_dir)
+    
+    got_dirs = os.listdir(dst_dir)
+    for got_dir in got_dirs:
+        if not os.path.isdir(os.path.join(dst_dir, got_dir)):
+            print(os.path.join(dst_dir, got_dir))
+            continue
+
+        subdirs = os.listdir(os.path.join(dst_dir, got_dir))
+        if len(subdirs) == 1:
+            mv_dir(os.path.join(dst_dir, got_dir, subdirs[0]), os.path.join(dst_dir, 'tmp'))
+            rm_dir(os.path.join(dst_dir, got_dir))
+            os.rename(os.path.join(dst_dir, 'tmp'), os.path.join(dst_dir, got_dir)) 
+    
+
     
 if __name__ == '__main__':
     main()
