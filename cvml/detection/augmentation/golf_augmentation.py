dac1cb1ab509089137273d5ae6bcea0ec0aceece
unknown <20alexchernik@gmail.com>
2022-11-05 23:41:58 +0300

    feature: maskmixup

--- cvml/detection/augmentation/golf_augmentation.py
+++ cvml/detection/augmentation/golf_augmentation.py
@@ -14,15 +14,17 @@ from typing import Tuple, List
 import albumentations as A
 
 
-class GOLF_Albumentations:
-    def __init__(self, crop_obj_dir: str, crop_class_names: str, class_names: List[str]):
-        self.dir_coco_obj = r"F:\PythonProjects\AnnotationConverter\datasets\defects_segment_27092022"
-        self.coco_class_names = ['comet']
-        self.class_names = ['comet']
+class MaskMixup:
+    def __init__(self, 
+                 crop_obj_dir: str, 
+                 crop_class_names: str, 
+                 class_names: List[str]):
 
-        self.coco_conformity = {
-            'comet': 0,
-        }
+        self.dir_coco_obj = crop_obj_dir
+        self.coco_class_names = crop_class_names
+        self.class_names = class_names
+
+        self.coco_conformity = {cls_name: i for i, cls_name in enumerate(class_names)}  # CHECK
 
         self.class_names_id = {class_name: class_id for class_id, class_name in enumerate(self.class_names)}
 
@@ -176,6 +178,7 @@ class GOLF_Albumentations:
             return None
 
     def select_obj(self):
+        # ENDLESS LOOP!!!
         while True:
             class_name = np.random.choice(list(self.coco_conformity.keys()))
             if (self.coco_conformity.get(class_name) is not None) and \
@@ -266,7 +269,8 @@ class GOLF_Albumentations:
                                          translate=0.4,
                                          scale=0.4,
                                          shear=30,
-                                         perspective=0.0) -> np.ndarray:
+                                         perspective=0.0,
+                                         scale_coef=0.5) -> np.ndarray:
         height = img_shape[0]
         width = img_shape[1]
 
@@ -284,7 +288,8 @@ class GOLF_Albumentations:
         R = np.eye(3)
         a = random.uniform(-degrees, degrees)
         # a += random.choice([-180, -90, 0, 90])  # add 90deg rotations to small rotations
-        s = random.uniform(1 - scale, 1 + scale)
+        s = random.uniform(1 - scale, 1 + scale) * scale_coef # special rescale
+        # s = random.uniform(1 - scale, 1 + scale)
         # s = 2 ** random.uniform(-scale, scale)
         R[:2] = cv2.getRotationMatrix2D(angle=a, center=(0, 0), scale=s)
 
@@ -303,7 +308,8 @@ class GOLF_Albumentations:
 
         return M
 
-    def warp_bbox(self, bbox: list, warp_mat: np.ndarray) -> list:
+    @staticmethod
+    def warp_bbox(bbox: list, warp_mat: np.ndarray) -> list:
         x1, y1, x2, y2 = bbox
         corners = np.single([[[x1, y1],
                               [x1, y2],
@@ -371,14 +377,14 @@ def read_labels(path: str) -> np.ndarray:
     with open(path) as f:
         rows = f.read().split('\n')
 
-    lines = np.zeros((0, 5))
+    lines = []
     for row in rows:
         if row == '':
             continue
         line = list(map(float, row.split(' ')))
-        np.append(lines, np.array([line]), axis=0)
+        lines.append(line)
 
-    return np.array(lines)
+    return np.array(lines).reshape(-1, 5)
 
 
 def write_labels(path: str, lines: np.ndarray):
@@ -473,6 +479,243 @@ def get_tube_crop(detector: Yolov5Detector, img: np.ndarray) -> np.ndarray:
     return tube_crop
 
 
+def test_line(img):
+    src = img.copy()
+    src = cv2.split(src)[2]
+    
+    src = cv2.GaussianBlur(src, (5, 5), 0)
+    dst = cv2.Canny(src, 50, 100, None, 3)
+    
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+    
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 60, None, 0, 0, np.pi*0.55, np.pi*0.7)
+    
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(cdst, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
+    
+    
+    linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+    
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 1, cv2.LINE_AA)
+    
+    cv2.imshow("Source", src)
+    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv2.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+    
+    cv2.waitKey(0)
+
+
+def constrain(value, lower_bound, upper_bound):
+    if value < lower_bound:
+        return lower_bound
+    elif value > upper_bound:
+        return upper_bound
+    return value
+
+
+def get_intersection(line1, line2):
+    """Finds the intersection of two lines given in Hesse normal form.
+
+    Returns closest integer pixel locations.
+    See https://stackoverflow.com/a/383527/5087436
+    """
+    rho1, theta1 = line1[0]
+    rho2, theta2 = line2[0]
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    x0, y0 = int(np.round(x0)), int(np.round(y0))
+    return [x0, y0]
+
+
+def find_line_bounds(line, height, width):
+    h_border_1 = [[0, 0.5 * np.pi]]
+    h_border_2 = [[height, 0.5 * np.pi]]
+    
+    v_border_1 = [[0, 0]]
+    v_border_2 = [[width, 0]]
+
+    rho, theta = line[0]
+    
+    points = []
+    
+    if abs(theta) < 0.001:
+        intersection_1 = get_intersection(line, h_border_1)
+        intersection_2 = get_intersection(line, h_border_2)
+
+        intersection_1[0] = constrain(intersection_1[0], 0, width)
+        intersection_2[0] = constrain(intersection_2[0], 0, width)
+
+        points = [intersection_1, intersection_2]
+    
+    elif abs(theta - 0.5 * np.pi) < 0.001:
+        intersection_1 = get_intersection(line, v_border_1)
+        intersection_2 = get_intersection(line, v_border_2)
+
+        intersection_1[1] = constrain(intersection_1[1], 0, height)
+        intersection_2[1] = constrain(intersection_2[1], 0, height)
+
+        points = [intersection_1, intersection_2]
+    else:
+        intersections = [0, 0, 0, 0]
+        intersections[0] = get_intersection(line, v_border_1)
+        intersections[1] = get_intersection(line, v_border_2)
+        intersections[2] = get_intersection(line, h_border_1)
+        intersections[3] = get_intersection(line, h_border_2)
+
+        for i in range(4):
+            x, y = intersections[i]
+            if 0 <= x <= width and 0 <= y <= height:
+                points.append([x, y])
+            
+            if len(points) == 2:
+                break
+
+    return points
+
+
+def find_lines_by_params(dst, min_theta, max_theta, min_rho_delta, max_rho_delta):
+    # 0.06 * np.pi, 0.18 * np.pi
+    lines = cv2.HoughLines(dst, 1, np.pi / 180, 63, None, 0, 0, min_theta, max_theta)    # -0.02 * np.pi, 0.02 * np.pi
+    lines = [] if lines is None else [[[lines[i][0][0], lines[i][0][1]]] for i in range(len(lines))]
+    lines.sort(key=lambda x: x[0][0])
+
+    target_lines = []
+    for i in range(len(lines)):
+        for j in range(i + 1, len(lines)):
+            rho1 = lines[i][0][0]
+            theta1 = lines[i][0][1]
+            
+            rho2 = lines[j][0][0]
+            theta2 = lines[j][0][1]
+
+            if min_rho_delta <= rho2 - rho1 <= max_rho_delta:
+                print(rho1, rho2)
+                target_lines = [lines[i], lines[j]] 
+                break
+
+    return target_lines
+
+
+def get_tube_crop(img: np.ndarray, labels: np.ndarray, height: int, width: int) -> np.ndarray:
+    src = img.copy()
+    gray = cv2.split(src)[2]
+    
+    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    dst = cv2.Canny(gray, 50, 100, None, 3)
+    
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+
+    params_list = [
+        {'min_theta': 0.06 * np.pi, 'max_theta': 0.18 * np.pi, 'min_rho_delta': 300, 'max_rho_delta': 500},
+        {'min_theta': -0.02 * np.pi, 'max_theta': 0.02 * np.pi, 'min_rho_delta': 300, 'max_rho_delta': 500},
+        {'min_theta': 0.55 * np.pi, 'max_theta': 0.7 * np.pi, 'min_rho_delta': 300, 'max_rho_delta': 500},
+    ]
+
+    for params in params_list:
+        target_lines = find_lines_by_params(dst, **params)
+        if len(target_lines) != 0:
+            break
+    
+    if len(target_lines) == 0:
+        return img, labels
+    
+
+    region_points = []
+    if target_lines is not None:
+        for i in range(0, len(target_lines)):
+            rho = target_lines[i][0][0]
+            theta = target_lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(cdst, pt1, pt2, (0,0,255), 1, cv2.LINE_AA)
+
+            ipt1, ipt2 = find_line_bounds(target_lines[i], cdst.shape[0], cdst.shape[1])
+            region_points.append(ipt1)
+            region_points.append(ipt2)
+            cv2.circle(cdst, (ipt1[0], ipt1[1]), 5, (0, 255, 0), -1)
+            cv2.circle(cdst, (ipt2[0], ipt2[1]), 5, (0, 255, 0), -1)
+    
+    if len(region_points) != 0:
+        src_region_points = np.array(region_points, dtype=np.float32)
+        dst_region_points = np.array([[0, 0], [0, height], [width, 0], [width, height]], dtype=np.float32)
+        
+        warp_mat = cv2.getPerspectiveTransform(src_region_points, 
+                                               dst_region_points)
+        tube_img = cv2.warpPerspective(src, warp_mat, (width, height))
+        
+        new_bboxes = []
+        for bbox in labels:
+            cls_id, x, y, w, h = bbox
+            bbox_xyxy = [(x - w/2) * width, (y - h/2) * height, (x + w/2) * width, (y + h/2) * height]
+            new_xyxy = MaskMixup.warp_bbox(bbox_xyxy, warp_mat)
+
+            x1, y1, x2, y2 = new_xyxy
+
+            x1 /= width
+            y1 /= height
+            x2 /= width
+            y2 /= height
+
+            if x2 < 0 or y2 < 0:
+                continue
+            if x1 > 1 or  y1 > 1:
+                continue
+            
+            x1 = max(0, x1)
+            x2 = min(1, x2)
+            y1 = max(0, y1)
+            y2 = min(1, y2)
+
+            w = x2 - x1
+            h = y2 - y1
+
+            new_bboxes.append([cls_id, x1 + w/2, y1 + h/2, w, h])
+        
+        cv2.imshow("Cropped Tube", tube_img)
+
+
+    cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv2.imshow("Source", gray)
+    
+    cv2.waitKey(0)
+
+    return tube_img, np.array(new_bboxes).reshape(-1, 5)
+
+
+def tube_augmentation(img: np.ndarray, 
+                      labels: np.ndarray, 
+                      augmenter: MaskMixup) -> Tuple[np.ndarray]:
+    height, width = img.shape[0]
+    tube_img, tube_labels = get_tube_crop(img, labels, height, width)
+    new_tube_img, new_tube_labels = augmenter(tube_img, tube_labels)
+
+    return new_tube_img, new_tube_labels
+
+
 
 if __name__ == '__main__':
 
@@ -480,7 +723,10 @@ if __name__ == '__main__':
     new_dataset_dir = r'D:\datasets\tmk_yolov5_17092022_aug'
     
     # detector = Yolov5Detector(r'E:\PythonProjects\AnnotationConverter\weights\yolov5l_tube.pt')
-    golf = GOLF_Albumentations()
+    dir_coco_obj = r"D:\datasets_old\defects_segment_27092022"
+    coco_class_names = ['comet']
+    class_names = ['comet']
+    golf = MaskMixup(crop_obj_dir=dir_coco_obj, crop_class_names=coco_class_names, class_names=class_names)
     
     splits = ['train']
     for split in splits:
@@ -499,7 +745,12 @@ if __name__ == '__main__':
             img_name = os.path.splitext(img_file)[0]
             print(split, img_name)
             img = cv2.imread(os.path.join(images_dir, img_file))
+            img = cv2.resize(img, (640, 535))
             labels = read_labels(os.path.join(labels_dir, img_name + '.txt'))
+            print('old_labels', labels)
+
+            img, labels = get_tube_crop(img, labels, img.shape[0], img.shape[1])
+            #test_line(img)
 
             # tube_img = get_tube_crop(detector, img)
             new_img, new_labels = golf(img, labels)
@@ -516,11 +767,13 @@ if __name__ == '__main__':
                             (x + w, y + h),
                             (0, 255, 0), 5)
         
-            cv2.imshow("test", cv2.resize(new_img, (400, 400)))
-            cv2.waitKey(0)
+            cv2.imshow("test", new_img)
+            if cv2.waitKey(0) == 27:
+                break
 
             # cv2.imwrite(os.path.join(new_images_dir, img_name + '_aug.jpg'), new_img)
             # write_labels(os.path.join(new_labels_dir, img_name + '_aug.txt'), new_labels)
 
             # break ### TMP
+
 
