dda1f3067c67af54293e965d204f47eabbe69116
unknown <20alexchernik@gmail.com>
2023-01-01 21:02:07 +0300

    refactor: delete old extractor, replace yolo io

--- cvml/detection/dataset/annotation_converter.py
+++ cvml/detection/dataset/annotation_converter.py
@@ -4,7 +4,6 @@ from typing import Callable, List, Dict
 
 from cvml.core.bounding_box import BoundingBox, CoordinatesType
 from cvml.detection.dataset.annotation import Annotation
-from cvml.detection.dataset.io_handling import read_yolo_labels
 
 
 class AnnotationConverter:
@@ -256,6 +255,42 @@ class AnnotationConverter:
                 coco_annotations.append(coco_annotation)
         return coco_annotations
 
+
+def read_yolo_labels(path: str, img_size: tuple) -> List[BoundingBox]:
+
+    image_name = os.path.splitext(os.path.split(path)[-1])[0]
+    with open(path, 'r', encoding='utf-8') as f:
+        rows = f.read().split('\n')
+        bboxes = []
+        for row in rows:
+            if row == '':
+                continue
+            row_data = list(map(float, row.split(' ')))
+            if len(row_data) == 5:
+                cls_id, xc, yc, w, h = row_data
+                # x = xc - w / 2
+                # y = yc - h / 2
+                cls_conf = None
+            else:  # 6
+                cls_id, xc, yc, w, h, cls_conf = row_data
+                # x = xc - w / 2
+                # y = yc - h / 2
+
+            bbox = BoundingBox(cls_id, xc, yc, w, h, cls_conf, 
+                               type_coordinates=CoordinatesType.Relative,
+                               image_name=image_name,
+                               img_size=img_size)
+            bboxes.append(bbox)
+    return bboxes
+
+
+def write_yolo_labels(path: str, bboxes: List[BoundingBox]):
+    with open(path, 'w') as f:
+        for bbox in bboxes:
+            cls_id = bbox.get_class_id()
+            xc, yc, w, h = bbox.get_relative_bounding_box()
+            line = f"{cls_id} {xc} {yc} {w} {h}\n"
+            f.write(line)
     
 
 
