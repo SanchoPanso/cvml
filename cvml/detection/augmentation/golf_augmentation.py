c1994bd6bc66257fd747802317d4a960a99362f7
unknown <20alexchernik@gmail.com>
2023-01-10 16:56:29 +0300

    feat: add class for applying maskmixup

--- cvml/detection/augmentation/golf_augmentation.py
+++ cvml/detection/augmentation/golf_augmentation.py
@@ -3,21 +3,17 @@ import sys
 import logging
 import math
 import random
-
-import torch
 import time
+from typing import Tuple, List
 
 import cv2
 import numpy as np
-
-from typing import Tuple, List
-import albumentations as A
 
 
 class MaskMixup:
     def __init__(self, 
                  crop_obj_dir: str, 
-                 crop_class_names: str, 
+                 crop_class_names: List[str], 
                  class_names: List[str]):
 
         self.dir_coco_obj = crop_obj_dir
@@ -394,80 +390,6 @@ def write_labels(path: str, lines: np.ndarray):
             f.write(' '.join(list(map(str, line))) + '\n')
 
 
-
-class Yolov5Detector:
-    def __init__(self, weights_path: str):
-        self.model = torch.hub.load('ultralytics/yolov5',
-                                    'custom',
-                                    path=weights_path,
-                                    force_reload=True)
-        self.duration = 0
-
-    def __call__(self, img: np.ndarray,
-                 count_part_x=1, count_part_y=1,
-                 size=640, conf=0.25, iou=0.45):
-
-        height, width = img.shape[0:2]
-        height_crop = int(height / count_part_y)
-        width_crop = int(width / count_part_x)
-        det_grapes = []
-
-        # cropping original image count_part_x*count_part_y parties and detection every parties
-        for part_x in range(count_part_x):
-            for part_y in range(count_part_y):
-                x0_crop = part_x * width_crop
-                y0_crop = part_y * height_crop
-                img_crop = img[y0_crop: y0_crop + height_crop,
-                           x0_crop: x0_crop + width_crop]
-
-                dets = self.inference(img_crop, size, conf, iou)  # xywh
-                dets[:, :4] = dets[:, :4].astype(int)
-
-                dets[:, 0] += x0_crop
-                dets[:, 1] += y0_crop
-
-                if dets.shape[0] != 0 and len(dets.shape) > 1:
-                    det_grapes.append(dets)
-        if len(det_grapes) == 0:
-            return np.zeros((0, 6))
-        return np.concatenate(det_grapes, axis=0)
-
-    def inference(self, img: np.ndarray, size=640, conf=0.25, iou=0.45) -> np.array:
-
-        self.model.conf = conf  # NMS confidence threshold
-        self.model.iou = iou  # NMS IoU threshold
-
-        self.duration = time.time()
-        results = self.model(img, size=size)
-        self.duration = time.time() - self.duration
-
-        df = results.pandas().xyxy[0]
-
-        xmin = np.array(df['xmin'])
-        xmax = np.array(df['xmax'])
-        ymin = np.array(df['ymin'])
-        ymax = np.array(df['ymax'])
-
-        x = xmin
-        y = ymin
-        w = xmax - xmin
-        h = ymax - ymin
-
-        bbox_xywh = np.stack([x, y, w, h]).T
-
-        cls_conf = np.array([df['confidence']]).T
-        cls_ids = np.array([df['class']]).T
-
-        if bbox_xywh.shape[0] == 0:
-            return np.zeros((0, 6))
-
-        output = np.concatenate([bbox_xywh, cls_conf, cls_ids], axis=1)
-
-        return output
-
-
-
-
 # def get_tube_crop(detector: Yolov5Detector, img: np.ndarray) -> np.ndarray:
 #     output = detector(img[:, :, ::-1])
 #     if output.shape[0] == 0:
@@ -700,14 +622,42 @@ def get_tube_crop(img: np.ndarray, labels: np.ndarray, height: int, width: int) 
     return False, img, labels
 
 
-def tube_augmentation(img: np.ndarray, 
-                      labels: np.ndarray, 
-                      augmenter: MaskMixup) -> Tuple[np.ndarray]:
-    height, width = img.shape[0]
-    tube_img, tube_labels = get_tube_crop(img, labels, height, width)
-    new_tube_img, new_tube_labels = augmenter(tube_img, tube_labels)
 
-    return new_tube_img, new_tube_labels
+
+class MaskMixupAugmentation:
+
+    def __call__(self, 
+                 mask_mixup_augmenter: MaskMixup, 
+                 img: np.ndarray, 
+                 labels: np.ndarray) -> Tuple[np.ndarray, np.ndarray] or None:
+        pass
+
+
+class MandrelAugmentation(MaskMixupAugmentation):
+
+    def __call__(self, 
+                 mask_mixup_augmenter: MaskMixup, 
+                 img: np.ndarray, 
+                 labels: np.ndarray) -> Tuple[np.ndarray, np.ndarray] or None:
+        
+        height, width = img.shape[0], img.shape[1]
+        ret, tube_img, tube_labels = get_tube_crop(img, labels, height, width)
+        if ret is False:
+            return None
+        
+        new_tube_img, new_tube_labels = mask_mixup_augmenter(tube_img, tube_labels)
+        return new_tube_img, new_tube_labels
+
+
+class TubeAugmentation(MaskMixupAugmentation):
+
+    def __call__(self, 
+                 mask_mixup_augmenter: MaskMixup, 
+                 img: np.ndarray, 
+                 labels: np.ndarray) -> Tuple[np.ndarray, np.ndarray] or None:
+        
+        new_tube_img, new_tube_labels = mask_mixup_augmenter(img, labels)
+        return new_tube_img, new_tube_labels
 
 
 
