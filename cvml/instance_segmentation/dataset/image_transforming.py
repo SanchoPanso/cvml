6e815bd71ae1c64393f9842d35dcc04811c0eb8c
unknown <20alexchernik@gmail.com>
2023-01-24 18:26:06 +0300

    fix: mask to contours

--- cvml/instance_segmentation/dataset/image_transforming.py
+++ cvml/instance_segmentation/dataset/image_transforming.py
@@ -5,7 +5,7 @@ from typing import List
 from array import array
 
 
-def get_mask_polygon(mask: np.ndarray) -> List[List[List[float]]]:
+def get_mask_contours(mask: np.ndarray) -> tuple:
     """
     Convert color mask into the set of points, which is the set of corners of apporximating polygon
     :mask: bgr image, in which black pixels - the absence of objects, other - the appearance of object
@@ -13,29 +13,22 @@ def get_mask_polygon(mask: np.ndarray) -> List[List[List[float]]]:
 
     gray_mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     ret, binary_mask = cv2.threshold(gray_mask, 1, 255, cv2.THRESH_BINARY)
-    edged_mask = cv2.Canny(binary_mask, 50, 150)
-    contours, hierarchy = cv2.findContours(edged_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-    points = []
-    for point in contours[0].tolist():
-        x, y = point[0]
-        points.append(x)
-        points.append(y)
+    contours, hierarchy = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    return points
+    return contours
 
 
 if __name__ == '__main__':
     img = np.zeros((400, 400, 3), dtype='uint8')
-    img = cv2.rectangle(img, (80, 80), (320, 320), (0, 0, 200), -1)
-    img = cv2.rectangle(img, (10, 10), (100, 100), (0, 0, 200), -1)
-    points = get_mask_polygon(img)
-
-    print(points)
-    for i in range(0, len(points), 2):
-        x1, y1 = points[i], points[i + 1]
-        x2, y2 = points[(i + 2) % len(points)], points[(i + 2) % len(points)]
-        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=2)            
+    img = cv2.rectangle(img, (80, 80), (180, 180), (0, 0, 200), -1)
+    img = cv2.rectangle(img, (50, 50), (150, 150), (0, 0, 200), -1)
+    
+    cv2.circle(img, (200, 200), 10, (0, 200, 0), -1)
+    
+    cv2.polylines(img, (np.array([[[300, 300], [320, 300], [330, 310], [320, 320], [310, 320]]], dtype=np.int32)), True, (200, 0, 0), 1)
+    
+    contours = get_mask_contours(img)
+    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)           
     
     cv2.imshow("test", img)
     cv2.waitKey()
