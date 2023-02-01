25a58c0da421b5e77cfe1780990845c85d191706
unknown <20alexchernik@gmail.com>
2023-02-02 00:18:12 +0300

    feat: add logger config

--- cvml/__init__.py
+++ cvml/__init__.py
@@ -2,3 +2,31 @@ __version__ = '0.1.1'
 
 import logging
 logging.getLogger('cvml').addHandler(logging.NullHandler())
+
+LOGGER_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": { 
+            "format": "%(asctime)s:%(name)s:%(process)d:%(lineno)d " "%(levelname)s %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },    
+    },
+    "handlers": {
+        "verbose_output": {
+            "formatter": "default",
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout", 
+        },
+    },
+    "loggers": {
+        "cvml": {
+            "level": "DEBUG",
+            "handlers": [
+                "verbose_output",
+            ],
+        },
+    },
+    "root": {},
+}
