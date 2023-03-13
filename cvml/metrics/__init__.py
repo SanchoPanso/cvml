845769517c0bbe06a62d7106d0c7d72ac6e9a5ed
unknown <20alexchernik@gmail.com>
2023-03-13 18:33:21 +0300

    fix: metrics

---
+++ cvml/metrics/__init__.py
@@ -0,0 +1,4 @@
+import sys
+import os
+
+from .detection.evaluator import DetectionEvaluator
--- cvml/metrics/detection/__init__.py
+++ cvml/metrics/detection/__init__.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-from .bounding_box import BoundingBox
-from .evaluator import DetectionEvaluator
-from .readers import get_bounding_boxes_from_dir
 
 
 
