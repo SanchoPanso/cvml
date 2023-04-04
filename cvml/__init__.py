2cc44c0d883cb2f5238e04ddd3fe90d266692076
student2 <20alexchernik@gmail.com>
2023-04-04 19:27:17 +0300

    feat: add func to get default prepared logger

--- cvml/__init__.py
+++ cvml/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.2.0'
 
 import logging
+import sys
 
 from cvml.annotation.annotation import Annotation
 
@@ -17,30 +18,48 @@ from cvml.dataset.detection_dataset import DetectionDataset
 from cvml.dataset.instance_segmentation_dataset import ISDataset
 
 logging.getLogger('cvml').addHandler(logging.NullHandler())
-LOGGER_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": { 
-            "format": "%(asctime)s:%(name)s:%(process)d:%(lineno)d " "%(levelname)s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },    
-    },
-    "handlers": {
-        "verbose_output": {
-            "formatter": "default",
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout", 
-        },
-    },
-    "loggers": {
-        "cvml": {
-            "level": "DEBUG",
-            "handlers": [
-                "verbose_output",
-            ],
-        },
-    },
-    "root": {},
-}
+# LOGGER_CONFIG = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "formatters": {
+#         "default": { 
+#             "format": "%(asctime)s:%(name)s:%(process)d:%(lineno)d " "%(levelname)s %(message)s",
+#             "datefmt": "%Y-%m-%d %H:%M:%S",
+#         },    
+#     },
+#     "handlers": {
+#         "verbose_output": {
+#             "formatter": "default",
+#             "level": "DEBUG",
+#             "class": "logging.StreamHandler",
+#             "stream": "ext://sys.stdout", 
+#         },
+#     },
+#     "loggers": {
+#         "cvml": {
+#             "level": "DEBUG",
+#             "handlers": [
+#                 "verbose_output",
+#             ],
+#         },
+#     },
+#     "root": {},
+# }
+
+
+def get_default_logger() -> logging.Logger:
+    cvml_logger = logging.getLogger('cvml')
+
+    # Create handlers
+    s_handler = logging.StreamHandler(sys.stdout)
+    s_handler.setLevel(logging.INFO)
+
+    # Create formatters and add it to handlers
+    s_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    s_handler.setFormatter(s_format)
+
+    # Add handlers to the logger
+    cvml_logger.addHandler(s_handler)
+
+    return cvml_logger
+
